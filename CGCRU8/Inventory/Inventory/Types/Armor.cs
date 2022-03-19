e7bcc0622be5a7b46adcff5681d88c79ffa8a3ca
Gondár Barna <gondarbarna@gmail.com>
2022-03-19 21:31:19 +0100

    Armor

---
+++ CGCRU8/Inventory/Inventory/Types/Armor.cs
@@ -0,0 +1,25 @@
+﻿namespace Inventory.Types
+{
+    internal record class Armor
+    {
+        public double PhysicalDefense { get; set; }
+        public double StrikeDefense { get; set; }
+        public double SlashDefense { get; set; }
+        public double ThrustDefense { get; set; }
+
+        public double MagicDefense { get; set; }
+        public double FireDefense { get; set; }
+        public double LightningDefense { get; set; }
+        public double DarkDefense { get; set; }
+        
+        public int BleedResistance { get; set; }
+        public int PoisonResistance { get; set; }
+        public int FrostResistance { get; set; }
+        public int CurseResistance { get; set; }
+
+        public double Poise { get; set; }
+
+        public int Durability { get; set; }
+        public double Weight { get; set; }
+    }
+}
---
+++ CGCRU8/Inventory/Inventory/Types/ArmorType.cs
@@ -0,0 +1,27 @@
+﻿namespace Inventory.Types
+{
+    internal class ArmorType
+    {
+        public virtual string? description { get; protected set; }
+    }
+
+    internal class Helmet : ArmorType
+    {
+        public override string description { get { return "Helms are Armor in Dark Souls 3 that protects the player's head. It is speculated that they will work the same way as previous Souls games, by simply adding their defense values to a total that is then used to calculate how much damage is taken. Click the header to sort"; } }
+    }
+
+    internal class Chest : ArmorType
+    {
+        public override string description { get { return "Chest Armors are Armor in Dark Souls 3 that protects the player's torso. Click the table header to sort"; } }
+    }
+
+    internal class Gauntlet : ArmorType
+    {
+        public override string description { get { return "Gauntlets are Armor in Dark Souls 3 that protects the player's arms. It is speculated that they will work the same way as previous Souls games, by simply adding their defense values to a total that is then used to calculate how much damage is taken. Click the table header to sort."; } }
+    }
+
+    internal class Legging : ArmorType
+    {
+        public override string description { get { return "Leggings or Leg Armor is Armor in Dark Souls 3 that protects the player's legs. They will work the same way as previous Souls games, by simply adding their defense values to a total that is then used to calculate how much damage is taken. Click the table header to sort."; } }
+    }
+}
