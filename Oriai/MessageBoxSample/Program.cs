ba71b1db326c6fd6c15dcebe591f495a538bcbcd
gabor.ruzsinszki <gabor.ruzsinszki@evosoft.com>
2022-04-07 11:29:11 +0200

    Pinvoke

---
+++ Oriai/MessageBoxSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MessageBoxSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MessageBoxWrapper.ShowMessageBox("Hello World 3.0", "Hello", MessageBoxType.OkCancel, MessageBoxIcon.Information);
+        }
+    }
+}
