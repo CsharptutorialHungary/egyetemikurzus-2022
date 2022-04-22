e4aae7c7297129f04a2c7fbb40a4dc54e3577bea
Horváth Krisztián <h046039@stud.u-szeged.hu>
2022-04-22 22:02:06 +0200

    Hajo osztály (attributumok, konstruktor, get/set)

--- MihalyPal_HorvathKrisztian/Torpedo/Beadando/Hajo.cs
+++ MihalyPal_HorvathKrisztian/Torpedo/Beadando/Hajo.cs
@@ -8,6 +8,31 @@ namespace Beadando
 {
     internal class Hajo
     {
+        private int X { get; set; }
+        private int Y { get; set; }
+
+        private int Tipus; 
+        private int Eletero { get; set; }
+
+        public Hajo(int x, int y, int tipus, int eletero)
+        {
+            X = x;  
+            Y = y;  
+            Tipus = tipus;  
+            Eletero = eletero;  
+
+        }
+        public Hajo()
+        {
+            
+        }
+        public void SetTipus(int tipus)
+        {
+            if(tipus >= 2 && tipus <= 5)
+            {
+                this.Tipus = tipus;
+            }
+        }
         //Attributumai egy x és y kezdőérték 
         //tipusa: 2,3,4,5
         //életerő: ami kezdetben a típussal egyezik meg de ez csökken
