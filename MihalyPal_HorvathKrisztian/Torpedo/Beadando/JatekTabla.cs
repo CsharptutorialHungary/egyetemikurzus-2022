905d4671e0c63d0e78dd78fa2a7c0d65a9adcd95
Mihály Pál <h048889@stud.u-szeged.hu>
2022-04-23 15:03:26 +0200

    main függvény program.cs

--- MihalyPal_HorvathKrisztian/Torpedo/Beadando/JatekTabla.cs
+++ MihalyPal_HorvathKrisztian/Torpedo/Beadando/JatekTabla.cs
@@ -8,17 +8,27 @@ namespace Beadando
 {
     internal class JatekTabla
     {
-        private int Szelesseg { get; set; }
-        private int Magassag { get; set; }
+        public int Szelesseg { get; set; }
+        public int Magassag { get; set; }
 
-        private Hajo [] Hajok { get; set; }  
-        private char [,]Palya { get; set; }
+        public Hajo[] Hajok { get; set; }
+        public char[,] Palya { get; set; }
         public JatekTabla(int szelesseg, int magassag)
         {
             Szelesseg = szelesseg;
             Magassag = magassag;
-            Palya = new char[szelesseg,magassag];
+            Palya = new char[magassag, szelesseg];
             Hajok = new Hajo[6]; //2-es 2 db, 3-as 2db, 4-es 1 db, 5-ös 1 db
+            for (int i = 0; i < Hajok.Length; i++)
+            {
+                Hajok[i] = new Hajo();
+            }
+            Hajok[0].SetTipus(2);
+            Hajok[1].SetTipus(2);
+            Hajok[2].SetTipus(3);
+            Hajok[3].SetTipus(3);
+            Hajok[4].SetTipus(4);
+            Hajok[5].SetTipus(5);
         }
 
         public void PalyatGeneral()
@@ -26,36 +36,52 @@ namespace Beadando
 
         {
 
-           for(int i = 0; i < Szelesseg; i++)
+            for (int i = 0; i < Magassag; i++)
             {
-                for (int j = 0; j < Magassag; j++)
+                for (int j = 0; j < Szelesseg; j++)
                 {
-                    Palya[i,j] = '~';
-                    Console.Write(" "+Palya[i,j]+" ");
+                    Palya[i, j] = '~';
+
                 }
-                Console.WriteLine();
+
             }
         }
 
         public void HajokatElhelyez()
         {
-            for (int i = 0; i < Szelesseg; i++)
+            //Hiba ellenőrzés ide is 
+            for (int k = 0; k < Hajok.Length; k++)
             {
-                for (int j = 0; j < Magassag; j++)
-                {
-                    for(int k = 0; k <Hajok.Length; k++)
-                    {
+                Palya[Hajok[k].X, Hajok[k].Y] = 'O';
 
-                        if (Hajok[k].X == i && Hajok[k].Y == j) {
-                            Palya[i, j] = 'O';
-                         }
-                    }
-                }
             }
         }
-        //Attributumai a pálya szélessége és magassága
-        //egy tömb ami hajokat tartalmaz
+        public void HajotLo(int x, int y)
+        {
+            for (int i = 0; i < Hajok.Length; i++)
+            {
+                if (Hajok[i].HajoKoordinataE(x, y) == true)
+                {
+                    Palya[x, y] = 'X';
+                }
+            }
+            //Attributumai a pálya szélessége és magassága
+            //egy tömb ami hajokat tartalmaz
 
-        //Metódusai: Pályát létrehoz, Hajot elhelyez, Hajótkilő, Hajót elsüllyedt-e,
+            //Metódusai: Pályát létrehoz, Hajot elhelyez, Hajótkilő, Hajót elsüllyedt-e,
+
+        }
+
+        public void tablatKiir()
+        {
+            for (int i = 0; i < Magassag; i++)
+            {
+                for (int j = 0; j < Szelesseg; j++)
+                {
+                    Console.Write(" " + Palya[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
--- MihalyPal_HorvathKrisztian/Torpedo/Beadando/Program.cs
+++ MihalyPal_HorvathKrisztian/Torpedo/Beadando/Program.cs
@@ -6,11 +6,32 @@ namespace Beadando
     {
         static void Main(string[] args)
         {
-            Hajo h = new Hajo();
-    
-            JatekTabla j = new JatekTabla(10,10);
+
+            Console.WriteLine("A hajók pozícióit úgy kell megadni, hogy megadod az X és Y koordinátákat, majd azt, hogy \n milyen" +
+                "irányban legyen elforgatva. Ezeknek a leírása a következő:\n" +
+                "1: ez az alapértelmezett, ilyenkor vízszintesen van elhelyezve a hajó.\n" +
+                "2: ez a függőleges elhelyezés.\n" +
+                "figyelj majd arra, hogy a hajók ne lógjanak ki a pályáról szóval az általad megadott pozíciótól\n" +
+                "mindig számolj el annyi blokkot, jobbra,vagy lefele amilyen típusú  a hajód!");
+
+            JatekTabla j = new JatekTabla(15, 10);
             j.PalyatGeneral();
 
+            AdatokatBeker bekeres = new AdatokatBeker();
+
+            for (int i = 0; i < j.Hajok.Length; i++)
+            {
+                bekeres.BekerHajot(j.Hajok[i]);
+            }
+            Boolean jatekban = true;
+
+            while (jatekban == true)
+            {
+
+            }
+            j.HajokatElhelyez();
+            Console.WriteLine();
+            j.tablatKiir();
         }
     }
 }
