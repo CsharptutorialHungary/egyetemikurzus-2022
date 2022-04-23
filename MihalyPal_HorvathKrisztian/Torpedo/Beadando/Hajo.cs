a9d00c607e72f9d79f79b6380db42c9de83481f8
Mihály Pál <h048889@stud.u-szeged.hu>
2022-04-23 14:57:28 +0200

    Hajó osztály

--- MihalyPal_HorvathKrisztian/Torpedo/Beadando/Hajo.cs
+++ MihalyPal_HorvathKrisztian/Torpedo/Beadando/Hajo.cs
@@ -8,6 +8,43 @@ namespace Beadando
 {
     internal class Hajo
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        private int Tipus;
+        public int Eletero { get; set; }
+
+        public Hajo(int x, int y, int tipus, int eletero)
+        {
+            X = x;
+            Y = y;
+            Tipus = tipus;
+            Eletero = eletero;
+
+        }
+        public Hajo()
+        {
+
+        }
+        public void SetTipus(int tipus)
+        {
+            if (tipus >= 2 && tipus <= 5)
+            {
+                this.Tipus = tipus;
+            }
+        }
+        public int GetTipus() { return this.Tipus; }
+        public Boolean HajoKoordinataE(int x, int y)
+        {
+            if (x == X && y == Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         //Attributumai egy x és y kezdőérték 
         //tipusa: 2,3,4,5
         //életerő: ami kezdetben a típussal egyezik meg de ez csökken
