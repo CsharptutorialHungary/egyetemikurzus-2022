882c58062591fdef1d0226284e058ed5fb78e93a
noelhorvath <hnoel2000@gmail.com>
2022-04-24 01:55:46 +0200

    Add Action and Cell classes

---
+++ JP0C9W/Amoba/Classes/Action.cs
@@ -0,0 +1,14 @@
+﻿using Amoba.Interfaces;
+
+namespace Amoba.Classes
+{
+    public class Action : IAction
+    {
+        public ICell SelectedCell { get; }
+        public Color PlayerColor { get; }
+        public Action(ICell cell, Color playerColor) {
+            SelectedCell = cell;
+            PlayerColor = playerColor;
+        }
+    }
+}
