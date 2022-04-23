c556f3ef9d9919fce855ce1809d0d345799d59c9
noelhorvath <hnoel2000@gmail.com>
2022-04-24 01:56:07 +0200

    update IGameEngine interface

---
+++ JP0C9W/Amoba/Interfaces/IGameEngine.cs
@@ -0,0 +1,13 @@
+﻿using Amoba.Classes;
+
+namespace Amoba.Interfaces
+{
+    public interface IGameEngine
+    {
+        IBoard Board { get; }
+        void StartNewGame();
+        void RequestMove();
+        bool IsMoveValid(int rowIndex, int colIndex);
+        GameStatus GetStatus();
+    }
+}
