600b4b910391bcc4d804a04ad229a887d561b9a6
Mihály Pál <h048889@stud.u-szeged.hu>
2022-04-23 15:01:17 +0200

    adatbekérő osztály

---
+++ MihalyPal_HorvathKrisztian/Torpedo/Beadando/AdatokatBeker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando
+{
+    internal class AdatokatBeker
+    {
+
+
+        public Hajo BekerHajot(Hajo h)
+        { //TODO hibaellenőrzés rossz inputra
+
+            Console.WriteLine("Kérlek helyezd el " + h.GetTipus() + " típusú hajódat.");
+            string poziciok = Console.ReadLine();
+            string[] splitPoziciok = poziciok.Split(' ');
+            h.X = Int32.Parse(splitPoziciok[0]);
+            h.Y = Int32.Parse(splitPoziciok[1]);
+            return h;
+        }
+        // bekérLövést
+        //bekérPályát (mekkora legyen)
+
+    }
+}
