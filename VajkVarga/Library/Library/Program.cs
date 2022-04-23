465dd720f464de40517bc538527dea8e2473ebde
Vajk Varga <vargavajkravag@gmail.com>
2022-04-23 12:59:00 +0200

    Initial commit with project

---
+++ VajkVarga/Library/Library/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
