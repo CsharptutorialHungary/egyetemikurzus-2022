18378085042e75c639d89f7e60ff174dd24ad67f
bencebolgovics <bolgovics.bence99@gmail.com>
2022-04-26 17:08:01 +0200

    Implementing basics of the subtraction and addition

--- IGRGE1/Calculator/ViewModel/MainViewModel.cs
+++ IGRGE1/Calculator/ViewModel/MainViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows.Input;
 
 namespace Calculator
@@ -12,14 +15,19 @@ namespace Calculator
     public class MainViewModel : ViewModelBase, INotifyPropertyChanged
     {
         private string totalText = "";
-
-        private string subTotalText = "123312";
+        private string subTotalText = "";
+        double resultHelper = 0;
         public string TotalText { get => totalText; set => SetProperty(ref totalText, value); }
         public string SubTotalText { get => subTotalText; set => SetProperty(ref subTotalText, value); }
         public ICommand NotZeroNumber_Command { get; private set; }
         public ICommand ZeroNumber_Command { get; private set; }
         public ICommand ClearTextBox_Command { get; private set; }
         public ICommand PowerTwo_Command { get; private set; }
+        public ICommand CommaSeparator_Command { get; private set; }
+        public ICommand WriteToMemory_Command { get; private set; }
+        public ICommand Addition_Command { get; private set; }
+        public ICommand ClearOneNumberFromTextBox_Command { get; private set; }
+        public ICommand Substraction_Command { get; private set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
@@ -39,24 +47,79 @@ namespace Calculator
             NotZeroNumber_Command = new RelayCommand<string>(NotZeroNumber_Click);
             ZeroNumber_Command = new RelayCommand(ZeroNumber_Click);
             ClearTextBox_Command = new RelayCommand(ClearTextBox);
-            PowerTwo_Command = new RelayCommand(PowerTwo);
+            CommaSeparator_Command = new RelayCommand(CommaSeparator);
+            WriteToMemory_Command = new RelayCommand(WriteToMemoryAdd);
+            Addition_Command = new RelayCommand(Addition);
+            ClearOneNumberFromTextBox_Command = new RelayCommand(ClearOneNumber);
+            Substraction_Command = new RelayCommand(Substraction);
         }
 
-        private void PowerTwo()
+        private void Substraction()
         {
-            string temp = TotalText;
-            if (TotalText.Length < 14)
+
+            var format = new NumberFormatInfo();
+            format.NegativeSign = "-";
+            format.NumberDecimalSeparator = ",";
+            if (!(TotalText.Equals("")) && SubTotalText.Equals(""))
             {
-                SubTotalText = temp + "*" + temp;
-                double mantissa = Math.Abs(Convert.ToDouble(TotalText));
-                mantissa *= mantissa;
-                TotalText = mantissa.ToString();
+                SubTotalText = TotalText + "-";
+                TotalText = "";
+                return;
+            }
+            if (!TotalText.Equals("") && !SubTotalText.Equals(""))
+            {
+                var numberFromSubTotalText = subTotalText.Split(new Char[] { '-', '+' });
+                double result = Convert.ToDouble(numberFromSubTotalText[0], format) - Convert.ToDouble(TotalText, format);
+                resultHelper = result;
+                SubTotalText = result.ToString() + "-";
+                TotalText = result.ToString();
             }
         }
 
-        private void ClearTextBox()
+        private void Addition()
+        {
+
+            var format = new NumberFormatInfo();
+            format.NegativeSign = "-";
+            format.NumberDecimalSeparator = ",";
+            if (!(TotalText.Equals("")) && SubTotalText.Equals("") )
+            {
+                SubTotalText = TotalText + "+";
+                TotalText = "";
+                return;
+            }
+            if(!TotalText.Equals("") && !SubTotalText.Equals(""))
+            {
+                var numberFromSubTotalText = subTotalText.Split(new Char[] { '-', '+' });
+                double result = Convert.ToDouble(numberFromSubTotalText[0],format) + Convert.ToDouble(TotalText,format);
+                resultHelper = result;
+                SubTotalText = result.ToString() + "+";
+                TotalText = result.ToString();
+            }
+        }
+
+        private async void WriteToMemoryAdd()
+        {
+           
+        }
+
+        private void CommaSeparator()
+        {
+            if(!TotalText.Equals("") && !TotalText.Contains(",") && TotalText.Length < 7)
+            {
+                TotalText += ",";
+            }
+        }
+
+        private  void  ClearTextBox()
         {
             TotalText = "";
+            SubTotalText = "";
+        }
+
+        private void ClearOneNumber()
+        {
+
         }
 
         private void ZeroNumber_Click()
@@ -73,11 +136,18 @@ namespace Calculator
 
         private void NotZeroNumber_Click(string obj)
         {
+
             if (TotalText == "0")
             {
                 TotalText = "";
                 TotalText += obj;
                 return;
+            }
+
+            if (resultHelper != 0)
+            {
+                TotalText = "";
+                resultHelper = 0;
             }
 
             if (TotalText.Length < 7)
