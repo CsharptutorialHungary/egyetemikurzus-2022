bb1ac25635a313bcfa0581a63d2a4316f90cc16c
gabor.ruzsinszki <gabor.ruzsinszki@evosoft.com>
2022-04-28 12:55:49 +0200

    További kódok

---
+++ Oriai/Task/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskSample
+{
+    internal class Program
+    {
+        public static async Task<long> Sum(int meddig, CancellationToken token)
+        {
+            long sum = 0;
+            for (int i = 0; i < meddig; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    return -1;
+                }
+                sum += i;
+                await Task.Delay(1000, token);
+            }
+            return sum;
+        }
+
+        private static CancellationTokenSource _tokenSource;
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                _tokenSource = new CancellationTokenSource();
+                Console.CancelKeyPress += OnCancelKey;
+
+
+                long result = await Sum(18888888, _tokenSource.Token);
+                Console.Write(result);
+            }
+            catch (TaskCanceledException)
+            {
+                Console.WriteLine("Türelmetlen voltál");
+            }
+            finally
+            {
+                _tokenSource.Dispose();
+            }
+        }
+
+        private static void OnCancelKey(object sender, ConsoleCancelEventArgs e)
+        {
+            _tokenSource.Cancel();
+            e.Cancel = true; //amúgy nem lépünk ki
+        }
+    }
+}
---
+++ Oriai/Thread2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Thread2
+{
+    internal class Program
+    {
+        private static object _lock = new();
+
+        private static void KulonSzal(object meddig)
+        {
+            int eddig = (int)(meddig);
+            for (int i = 0; i < eddig; i++)
+            {
+                lock (list)
+                {
+                    list.Add(i);
+                }
+            }
+        }
+
+        private static List<int> list = new List<int>();
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(KulonSzal);
+            t.Start(10);
+            Thread t2 = new Thread(KulonSzal);
+            t2.Start(12);
+
+            Thread.Sleep(1000);
+        }
+    }
+}
