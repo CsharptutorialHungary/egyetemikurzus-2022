6f7fb93b721ff1eca4fecf5112a946abe2110b2f
Peter <peter61045720@gmail.com>
2022-04-29 16:41:44 +0200

    Added model class for course

---
+++ E8G0NI/CourseManager/CourseManager/Models/CourseModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManager.Models
+{
+    public sealed record class CourseModel(string code, string name, string lecturer, int credit, int limit, string language, string description);
+}
