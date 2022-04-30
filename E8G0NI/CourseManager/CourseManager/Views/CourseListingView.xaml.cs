4e636253b589b63a372de9b3a30bac6fcb79b825
Peter <peter61045720@gmail.com>
2022-04-30 21:24:41 +0200

    Created Views

---
+++ E8G0NI/CourseManager/CourseManager/Views/CourseListingView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CourseManager.Views
+{
+    /// <summary>
+    /// Interaction logic for CourseListingView.xaml
+    /// </summary>
+    public partial class CourseListingView : UserControl
+    {
+        public CourseListingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
