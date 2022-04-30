31fbd81977a3c978d6981a4d3b840484d42e49ab
Peter <peter61045720@gmail.com>
2022-04-30 21:02:07 +0200

    Working Model layer

--- E8G0NI/CourseManager/CourseManager/App.xaml.cs
+++ E8G0NI/CourseManager/CourseManager/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CourseManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,12 @@ namespace CourseManager
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            CourseModel course = new CourseModel("IB500g", "Programming Basics", "Nagy József", 4, 60, "Hungarian", "");
+            MessageBox.Show(course.Name);
+
+            base.OnStartup(e);
+        }
     }
 }
