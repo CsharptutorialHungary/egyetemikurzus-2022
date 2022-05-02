cf4d3c996aaef2c93d1e7ef7aaeaec56c69837e9
Peter <peter61045720@gmail.com>
2022-05-02 22:49:53 +0200

    Check the text fields before submitting

--- E8G0NI/CourseManager/CourseManager/Commands/CreateCourseCommand.cs
+++ E8G0NI/CourseManager/CourseManager/Commands/CreateCourseCommand.cs
@@ -2,9 +2,11 @@
 using CourseManager.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CourseManager.Commands
 {
@@ -17,6 +19,19 @@ namespace CourseManager.Commands
         {
             _createCourseViewModel = createCourseViewModel;
             _course = courseModel;
+
+            _createCourseViewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return !string.IsNullOrEmpty(_createCourseViewModel.Code) && 
+                !string.IsNullOrEmpty(_createCourseViewModel.Name) &&
+                !string.IsNullOrEmpty(_createCourseViewModel.Lecturer) &&
+                _createCourseViewModel.Credit > 0 &&
+                _createCourseViewModel.Limit > 0 &&
+                !string.IsNullOrEmpty(_createCourseViewModel.Language) &&
+                base.CanExecute(parameter);
         }
 
         public override void Execute(object? parameter)
@@ -28,6 +43,21 @@ namespace CourseManager.Commands
                 _createCourseViewModel.Limit,
                 _createCourseViewModel.Language,
                 _createCourseViewModel.Description);
+
+            MessageBox.Show("Created course successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(CourseViewModel.Code) ||
+                e.PropertyName == nameof(CourseViewModel.Name) ||
+                e.PropertyName == nameof(CourseViewModel.Lecturer) ||
+                e.PropertyName == nameof(CourseViewModel.Credit) ||
+                e.PropertyName == nameof(CourseViewModel.Limit) ||
+                e.PropertyName == nameof(CourseViewModel.Language))
+            {
+                OnCanExecutedChanged();
+            }
         }
     }
 }
