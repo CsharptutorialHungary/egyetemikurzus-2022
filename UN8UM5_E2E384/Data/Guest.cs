19e832b800ad8a3090126de05abea8d63863cfe7
Loósz Roland Gábor <h984129@stud.u-szeged.hu>
2022-05-04 00:26:10 +0200

    TestCases added

--- UN8UM5_E2E384/Data/Guest.cs
+++ UN8UM5_E2E384/Data/Guest.cs
@@ -10,12 +10,46 @@ namespace Szalloda.Data
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Age { get; set; }
+
+        public int _age;
+        public int Age { get; }
+        public void SetAge()
+        {
+            if (Age <= 18)
+            {
+                _age = 18;
+            }
+            else if (Age > 99)
+            {
+                _age = 99;
+            }
+        }
         public int ReservedRoomId { get; set; }
-        public int Adults { get; set; }
-        public int Children { get; set; }
+
+        public int _adults;
+        public int Adults { get; }
+        public void SetAdults()
+        {
+            if (Adults <= 0)
+            {
+                _adults = 1;
+            }
+        }
+
+        public int _children;
+        public int Children { get; }
+        public void SetChildren()
+        {
+            if (Children < 0)
+            {
+                _children = 0;
+            }
+
+        }
+
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
+        // MailAddress m = new MailAddress(emailaddress);
         public DateTime ArrivalDate { get; set; }
         public DateTime DepartureDate { get; set; }
     }
