d38525284e2681a75dfd329bdaf887092c6ecc7c
csmartin99 <csajbok.martin2@gmail.com>
2022-05-03 11:10:04 +0200

    Serialize & Deserialize

--- UN8UM5_E2E384/App/App.cs
+++ UN8UM5_E2E384/App/App.cs
@@ -34,6 +34,7 @@ Console.WriteLine("\nBooking: ");
 GetGuestInformation();
 
 void GetGuestInformation() {
+    int guestListLength = 0;
     string name = Utility.GetUserInput("Name: ");
     int age = Utility.GetUserInputInt("Age: ");
     int reservedroom = Utility.GetUserInputInt("Room number: ");
@@ -45,20 +46,30 @@ void GetGuestInformation() {
     DateTime departure = Utility.GetUserInputDate("Departure (YYYY/MM/DD): ");
 
     //teszt
-    List<Guest> guestList = new List<Guest>
-            {
-                // TODO : folytatni
-                new Guest{Id=1, Name="Zsak Imre", Age=22, ReservedRoomId=1, Adults=2, Children=1, PhoneNumber="06305552233", Email="zsakosfrodo123@gmail.com",
-                    ArrivalDate= new DateTime(2022, 10, 10), DepartureDate= new DateTime(2022, 10, 12)},
-            };
+    List<Guest> guestList = new List<Guest> { };
 
+    if (File.Exists(@"../../../../Reservations.json"))
+    {
+        try
+        {
+            string oldGuestList = File.ReadAllText(@"../../../../Reservations.json");
+            guestList = JsonSerializer.Deserialize<List<Guest>>(oldGuestList);
+        }
+        catch (Exception ex) when (ex is IOException || ex is JsonException)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        guestListLength = guestList.Count;
+    }
+     
     guestList.Add(new Guest
     {
-        Id = 2,
+        Id = guestListLength + 1,
         Name = name,
         Age = age,
         ReservedRoomId = reservedroom,
         Adults = adults,
+        Children = children,
         PhoneNumber = phonenumber,
         Email = email,
         ArrivalDate = arrival,
