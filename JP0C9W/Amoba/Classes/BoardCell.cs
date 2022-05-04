e6d59e97df58da75624508f998fc7b2ea90b3555
noelhorvath <hnoel2000@gmail.com>
2022-05-05 00:50:25 +0200

    add Coordinate and BoardCell models

---
+++ JP0C9W/Amoba/Classes/BoardCell.cs
@@ -0,0 +1,29 @@
+﻿using Amoba.Interfaces;
+
+namespace Amoba.Classes
+{
+    public record class BoardCell : IBoardCell
+    {
+        public ICoordinate Coordinate { get; init; }
+        public BoardCellValue Value { get; init; }
+        public BoardCell(int x, int y, BoardCellValue value)
+        {
+            Coordinate = new RecordCoordinate(x < 0 ? 0 : x, y < 0 ? 0 : y);
+            Value = value;
+        }
+        public BoardCell(int x) : this(x, 0, BoardCellValue.EMPTY) { }
+        public BoardCell(BoardCellValue value) : this(0, 0, value) { }
+        public BoardCell() : this(0, 0, BoardCellValue.EMPTY) { }
+        public BoardCell(ICoordinate coordinate, BoardCellValue value)
+        {
+            Coordinate = new RecordCoordinate(coordinate.X < 0 ? 0 : coordinate.X, coordinate.Y < 0 ? 0 : coordinate.Y);
+            Value = value;
+        }
+        public BoardCell(ICoordinate coordinate) : this(coordinate, BoardCellValue.EMPTY) { }
+
+        public override string ToString()
+        {
+            return $"Color: {Value}, Row: {Coordinate.Y + 1}, Column: {Coordinate.X + 1}";
+        }
+    }
+}
---
+++ JP0C9W/Amoba/Interfaces/IBoardCell.cs
@@ -0,0 +1,10 @@
+﻿namespace Amoba.Interfaces
+{
+    public enum BoardCellValue
+    {
+        EMPTY = '#',
+        WHITE = 'O',
+        BLACK = 'X'
+    }
+    public interface IBoardCell : ICell<BoardCellValue> { }
+}
