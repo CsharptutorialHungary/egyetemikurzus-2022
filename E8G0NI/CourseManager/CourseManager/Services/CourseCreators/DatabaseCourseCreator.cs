a89e6986a724b987b00888c6aed24913db1d0cdb
Peter61045720 <peter61045720@gmail.com>
2022-05-04 14:53:03 +0200

    Setting up database (2)

--- E8G0NI/CourseManager/CourseManager/Services/CourseCreators/DatabaseCourseCreator.cs
+++ E8G0NI/CourseManager/CourseManager/Services/CourseCreators/DatabaseCourseCreator.cs
@@ -1,4 +1,6 @@
-﻿using CourseManager.Models;
+﻿using CourseManager.DbContexts;
+using CourseManager.DTOs;
+using CourseManager.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,36 @@ namespace CourseManager.Services.CourseCreators
 {
     public class DatabaseCourseCreator : ICourseCreator
     {
-        public Task CreateCourse(CourseModel course)
+        private readonly CourseDbContextFactory _dbContextFactory;
+
+        public DatabaseCourseCreator(CourseDbContextFactory dbContextFactory)
         {
-            throw new NotImplementedException();
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task CreateCourse(CourseModel course)
+        {
+            using (CourseDbContext context = _dbContextFactory.CreateDbContext())
+            {
+                CourseDTO courseDTO = ToCourseDTO(course);
+
+                context.Courses.Add(courseDTO);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        private CourseDTO ToCourseDTO(CourseModel course)
+        {
+            return new CourseDTO()
+            {
+                Code = course.Code,
+                Name = course.Name,
+                Lecturer = course.Lecturer,
+                Credit = course.Credit,
+                Limit = course.Limit,
+                Language = course.Language,
+                Description = course.Description,
+            };
         }
     }
 }
