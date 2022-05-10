9ed2d5bd3e741d9894f7bb8532b8aaa0dfcf014c
noelhorvath <hnoel2000@gmail.com>
2022-05-10 17:00:51 +0200

    Update Board

--- JP0C9W/Amoba/Classes/Board.cs
+++ JP0C9W/Amoba/Classes/Board.cs
@@ -28,6 +28,33 @@ namespace Amoba.Classes
             }
         }
         public IEnumerable<char[]> Cells { get; set; }
+
+        public static string BoardCellsToString(IEnumerable<char[]> cells)
+        {
+            string formattedBoard = "";
+            if (!cells.Any())
+                throw new ArgumentException("Can't convert cells to string, bacause cells argument is empty!");
+
+            int boardSize = cells.Count();
+            int rowNumTextOffset = (int)Math.Log10(boardSize);
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    if (j == 0)
+                    {
+                        for (int k = 0; k < rowNumTextOffset - Math.Log10(i + 1); k++)
+                        {
+                            formattedBoard += ' ';
+                        }
+                        formattedBoard += $"{i + 1}  ";
+                    }
+                    formattedBoard += cells.ElementAt(i)[j];
+                    formattedBoard += j != boardSize - 1 ? " " : '\n';
+                }
+            }
+            return formattedBoard;
+        } 
         public Board(int minSize, int maxSize, int boardSize, char emptyCell)
         {
             MinSize = minSize;
@@ -58,35 +85,15 @@ namespace Amoba.Classes
 
         public void SetCell(IBoardCell cell)
         {
-            if (0 <= cell.Coordinate.Y && cell.Coordinate.Y < BoardSize && 0 <= cell.Coordinate.X && cell.Coordinate.X < BoardSize)
-                Cells.ElementAt(cell.Coordinate.Y)[cell.Coordinate.X] = (char)cell.Value;
+            if (0 <= cell.Y && cell.Y < BoardSize && 0 <= cell.X && cell.X < BoardSize)
+                Cells.ElementAt(cell.Y)[cell.X] = (char)cell.Value;
             else
                 throw new ArgumentException("Invalid cell!");
         }
 
         public override string ToString()
         {
-            string formattedBoard = "";
-            int rowNumTextOffset = (int)Math.Log10(BoardSize);
-            //int colNumTextOffset = (int)Math.Log10(BoardSize);
-            //char[][] colNumText = new char[colNumTextOffset][];
-            for (int i = 0; i < BoardSize; i++)
-            {
-                for (int j = 0; j < BoardSize; j++)
-                {
-                    if (j == 0)
-                    {
-                        for (int k = 0; k < rowNumTextOffset - Math.Log10(i + 1); k++)
-                        {
-                            formattedBoard += ' ';
-                        }
-                        formattedBoard += $"{i + 1}  ";
-                    }
-                    formattedBoard += Cells.ElementAt(i)[j];
-                    formattedBoard += j != BoardSize - 1 ? " " : '\n';
-                }
-            }
-            return formattedBoard;
+            return BoardCellsToString(Cells);
         }
 
         public bool IsFilled()
@@ -99,5 +106,19 @@ namespace Amoba.Classes
             }
             return true;
         }
+
+        public IEnumerable<char[]> CopyCells()
+        {
+            var res = new char[BoardSize][];
+            for (int i = 0; i < BoardSize; i++)
+            {
+                res[i] = new char[BoardSize];
+                for (int j = 0; j < BoardSize; j++)
+                {
+                    res[i][j] = Cells.ElementAt(i)[j];
+                }
+            }
+            return res;
+        } 
     }
 }
