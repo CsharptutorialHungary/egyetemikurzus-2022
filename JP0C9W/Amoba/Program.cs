62d700e42f0ab6cda0efbc112d036a1184ca544c
noelhorvath <hnoel2000@gmail.com>
2022-05-12 03:26:29 +0200

    Update Program

--- JP0C9W/Amoba/Program.cs
+++ JP0C9W/Amoba/Program.cs
@@ -1,78 +1,120 @@
 ﻿using Amoba.Classes;
+using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Amoba.Tests")]
 namespace Amoba
 {
     internal class Program
     {
-        private static int? GameMode;
-        private static int? BoardSize;
-        private static string? Mode;
-        public static readonly string PLAY_MODE = "play";
-        public static readonly string REPLAY_MODE = "replay";
-        static int Main(string[] args)
+        internal static void StartEngine(int gameMode, int boardSize)
         {
-            if (args.Length == 0 || args.Length == 1 || 3 < args.Length)
+            try
             {
-                Console.WriteLine("Invalid arguments!\nExample usages:\n\n     Play a new game: play size_of_board game_mode\n     Replay a saved game: replay path_to_game_file");
-                return 1;
+                var mode = GameEngine.IntToGameMode(gameMode);
+                var reporter = new GameReporter(mode);
+                var engine = new GameEngine(reporter, boardSize);
+                engine.StartNewGame(mode);
             }
-            if (!GameEngine.IsValidEngineMode(args[0]))
+            catch (Exception ex)
             {
-                Console.WriteLine($"{args[0]} is an invalid mode!\nAvailable modes: play, replay");
-                return 2;
+                Console.WriteLine($"GameEngine error: {ex.Message}");
             }
-            Mode = args[0];
-            if (args.Length == 3)
-            {
-                bool boardSizeParseRes = int.TryParse(args[1], out int boardSize);
-                if (!boardSizeParseRes)
-                {
-                    Console.WriteLine("The required board size is an invalid integer!");
-                    return 3;
-                }
-                bool gameModeParseRes = int.TryParse(args[2], out int gameMode);
-                if (!gameModeParseRes || gameMode < 0 || 2 < gameMode)
-                {
-                    Console.WriteLine("The required game mode is invalid!");
-                    return 4;
-                }
-                BoardSize = boardSize;
-                GameMode = gameMode;
-            }
-            
-            if (GameMode.HasValue && BoardSize.HasValue && Mode == GameEngine.PLAY_MODE)
+        }
+
+        internal static void StartReporter(string file, int pauseTime)
+        {
+            var reporter = new GameReporter();
+            var task = Task.Run(async () =>
             {
                 try
                 {
-                    var gameMode = GameEngine.IntToGameMode(GameMode.Value);
-                    var reporter = new GameReporter(gameMode);
-                    var engine = new GameEngine(reporter, BoardSize.Value);
-                    engine.StartNewGame(gameMode);
-                } 
+                    await reporter.ReplayGameAsync(file, pauseTime);
+                }
                 catch (Exception ex)
                 {
-                    Console.Error.WriteLine($"GameEngine error: {ex.Message}");
+                    Console.WriteLine($"GameReporter error: {ex.Message}");
                 }
-                
-            } 
+            }
+            );
+            task.Wait();
+        }
+
+        internal static void Initialize(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                throw new Exception(
+                    "Invalid arguments!\n" +
+                    "Guide:\n\n" +
+                    "   Play a new game: play size_of_board game_mode\n" +
+                    "   Replay a saved game: replay path_to_game_file pause_time_between_turns_in_ms\n" +
+                    "\n" +
+                    "   Game modes:\n\n" +
+                    "       0 - Local multiplayer\n" +
+                    "       1 - Against random AI\n" +
+                    "       2 - Random AI vs random AI\n" +
+                    "\n" +
+                    "   Board sizes: 5-100\n" +
+                    "\n" +
+                    "   Rules: First player to place 5 symbols (O or X) in a line (row, column, diagonal) wins!" 
+                );
+            }
+
+            if (!GameEngine.IsValidEngineMode(args[0]))
+            {
+                throw new Exception($"{args[0]} is an invalid mode!\nCommands: play, replay");
+            }
+            var mode = args[0];
+
+            if (mode == GameEngine.PLAY_MODE)
+            {
+                bool boardSizeParseResult = int.TryParse(args[1], out int boardSize);
+                if (!boardSizeParseResult)
+                {
+                    throw new Exception("The required board size is an invalid integer!");
+                }
+
+                bool gameModeParseResult = int.TryParse(args[2], out int gameMode);
+                if (!gameModeParseResult || gameMode < 0 || 2 < gameMode)
+                {
+                    throw new Exception(
+                        "The required game mode is invalid!" +
+                        "Game modes:\n\n" +
+                        "   0 - Local multiplayer\n" +
+                        "   1 - Against random AI\n" +
+                        "   2 - Random AI vs random AI\n");
+                }
+
+                StartEngine(gameMode, boardSize);
+            }
             else
             {
-                var reporter = new GameReporter();
-                var task = Task.Run( async () => 
-                    {
-                        try
-                        {
-                            await reporter.ReplayGameAsync(args[1]);
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.Error.WriteLine($"GameReporter error: {ex.Message}");
-                        }
-                        
-                    } 
-                );
-                task.Wait();
-            } 
+                if (!File.Exists(args[1]))
+                {
+                    throw new Exception("Game file doesn't exists!");
+                }
+
+                bool timeoutResult = int.TryParse(args[2], out int timeout);
+                if (!timeoutResult || timeout < 0)
+                {
+                    throw new Exception($"{args[2]} is an invalid pause time!");
+                }
+
+                StartReporter(args[1], timeout);
+            }
+        }
+
+        internal static int Main(string[] args)
+        {
+            try
+            {
+                Initialize(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
             
             return 0;
         }
